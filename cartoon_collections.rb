6468f5a9421d6d908482f2b8d24240725fe8b090
Hessam Imen <hessam.imen@gmail.com>
2022-06-15 12:15:38 +1000

    Done with assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+#===== with EACH ========
+# def roll_call_dwarves(dwarves)
+#   arr =[]
+#   dwarves.each.with_index(1) do |dwarve, index|
+#       arr << "#{index} #{dwarve}"
+#   end
+#     puts arr
+# end
+#===== with MAP ========
+def roll_call_dwarves(dwarves)
+  dwarves.map.with_index(1) do |dwarve, index|
+      puts "#{index} #{dwarve}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(calls)
+  calls.map do |call|
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.size > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  strings.detect do |string|
+    cheese_types.include?(string)
+  end
 end
